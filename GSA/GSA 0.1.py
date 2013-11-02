6dcf88712e9d0ac431f75b11dcee9a1d1c62226f
Matko <matkezicro@gmail.com>
2013-11-02 19:03:13 +0100

    pocetak citanja ulazne

--- GSA/GSA 0.1.py
+++ GSA/GSA 0.1.py
@@ -8,6 +8,25 @@ def main ():
     print "HelloWorld"
     print "Ovo je promjena"
 
+    ulaz = open('Ulazna.txt','r')
+
+    #učitavanje prva 3 reda
+
+    nezavrsni = ulaz.readline().split(' ')
+    del nezavrsni[0]
+    nezavrsni [-1] = nezavrsni[-1].strip()
+    pocetni_nezavrsni = nezavrsni[0]
+
+    zavrsni = ulaz.readline().split(' ')
+    del zavrsni[0]
+    zavrsni [-1] = zavrsni[-1].strip()
+
+    sinkronizacijski = ulaz.readline().split(' ')
+    del sinkronizacijski[0]
+    sinkronizacijski [-1] = sinkronizacijski[-1].strip()
+
+
+
 
 
 if __name__ == '__main__':
