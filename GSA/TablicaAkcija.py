c69e924907ef3b71ac7e4ecad82291f5685e20eb
MatkeziCro <matkezicro@gmail.com>
2013-11-12 10:19:49 +0100

    sreden DKA

---
+++ GSA/TablicaAkcija.py
@@ -0,0 +1,8 @@
+"""_
+@author: Matko
+"""
+
+class TablicaAkcija(object):
+
+    def __init__(self):
+        pass
---
+++ GSA/TablicaNovoStanje.py
@@ -0,0 +1,8 @@
+"""_
+@author: Matko
+"""
+
+class TablicaNovoStanje(object):
+
+    def __init__(self):
+        pass
